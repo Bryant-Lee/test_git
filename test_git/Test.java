9651e57ff2c566f60584bc5ed0f8662194987efa
my name <myemailid@domain.com>
2017-06-10 20:13:34 +0800

    devepment on the branch dev

--- test_git/Test.java
+++ test_git/Test.java
@@ -1,5 +1,6 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("push the file to the remote responsity");
+		System.out.println("Create a new branch is quick");
 	}
 }
