ddbdf06c0d86027a378dc04dc0d604b956dc9596
Bryant-Lee <896343259@qq.com>
2017-06-14 14:23:53 +0800

    modify the file by cqli

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("push the file to the remote responsity");
 		System.out.println("Create a new branch is quick and simple");
+		System.out.println("make a change of the Test.java file");
 	}
 }
