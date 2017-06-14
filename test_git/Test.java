70cd96425efa349dd5c7761514d8fd678c631c7d
Bryant-Lee <896343259@qq.com>
2017-06-14 14:47:48 +0800

    delete two file never use

--- test_git/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-	public static void main(String[] args){
-		System.out.println("push the file to the remote responsity");
-		System.out.println("Create a new branch is quick and simple");
-	}
-}
